7c6bfcf6c82c7a3a7c785ddfe1f215b9b3c436aa
Joel Gustafson <joelg@mit.edu>
2024-01-06 21:06:29 -0500

    Fix src/lib.zig exports

--- src/lib.zig
+++ src/lib.zig
@@ -1,6 +1,6 @@
 pub const Database = @import("Database.zig");
 pub const Method = @import("Method.zig").Method;
-pub const Query = @import("Method.zig").Query;
+pub const Query = @import("Query.zig").Query;
 
 const types = @import("types.zig");
 pub const Blob = types.Blob;
